6736006c2ace3edbd7f4e5716a41b5cf3f60d564
dohpark <dohpark1129@gmail.com>
2025-01-11 19:39:20 +0900

    feat: createElement 함수 구현

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div id="app">
+      <h1>Hello, React Clone!</h1>
+      <h2>Hello, Babel!</h2>
+      <div id="test">
+        <p>Hello, Test 1!</p>
+        <p>Hello, Test 2!</p>
+      </div>
+    </div>
+  );
+}
