682aa599c6c8f7e37208b7e18ef5488ebdce006c
dohpark <dohpark1129@gmail.com>
2025-01-22 23:08:32 +0900

    feat: 카운터 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { VirtualDom } from '@react/types';
+import { useState } from '@react/useState';
 
 function H1() {
   return <h1>Hello, React!</h1>;
@@ -6,6 +7,30 @@ function H1() {
 
 function H2() {
   return <h2>Hello, Babel!</h2>;
+}
+
+function PositiveCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
+
+function NegativeCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
 }
 
 export default function App(): VirtualDom {
@@ -17,6 +42,8 @@ export default function App(): VirtualDom {
         <p>Hello, Test 1!</p>
         <p>Hello, Test 2!</p>
       </div>
+      <PositiveCounter />
+      <NegativeCounter />
     </div>
   );
 }
